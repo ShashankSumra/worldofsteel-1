68f77d5200f76a27f7c4ab13a0bc6808a7220ada
ShashankSumra <shashank@worldofsteel.com>
2016-10-12 15:57:50 +0530

    NewChanges
    New Changes

--- www/js/app.js
+++ www/js/app.js
@@ -134,6 +134,141 @@ angular.module('starter', ['ionic', 'starter.controllers'])
 		  controller: "emailCtrl"
         }
       }
+    })
+	
+	.state('app.learning_center', {
+      url: "/learning_center",
+      views: {
+        'menuContent' :{
+          templateUrl: "templates/learning_center.html"
+        }
+      }
+    })
+    
+    .state('app.packaging_steel_conversion_chart', {
+      url: "/learning/packaging_steel_conversion_chart",
+      views: {
+        'menuContent' :{
+          templateUrl: "templates/learning/packaging_steel_conversion_chart.html"
+        }
+      }
+    })
+    
+    .state('app.thickness_conversion_chart', {
+      url: "/learning/thickness_conversion_chart",
+      views: {
+        'menuContent' :{
+          templateUrl: "templates/learning/thickness_conversion_chart.html"
+        }
+      }
+    })
+    
+    .state('app.raw_materials_grades', {
+      url: "/learning/raw_materials_grades",
+      views: {
+        'menuContent' :{
+          templateUrl: "templates/learning/raw_materials_grades.html"
+        }
+      }
+    })
+    
+    .state('app.bars_shapes_and_steel_plates_grades', {
+      url: "/learning/bars_shapes_and_steel_plates_grades",
+      views: {
+        'menuContent' :{
+          templateUrl: "templates/learning/bars_shapes_and_steel_plates_grades.html"
+        }
+      }
+    })
+    
+    .state('app.steel_plates_and_sheets_grades', {
+      url: "/learning/steel_plates_and_sheets_grades",
+      views: {
+        'menuContent' :{
+          templateUrl: "templates/learning/steel_plates_and_sheets_grades.html"
+        }
+      }
+    })
+    
+    .state('app.steel_pipes_grades', {
+      url: "/learning/steel_pipes_grades",
+      views: {
+        'menuContent' :{
+          templateUrl: "templates/learning/steel_pipes_grades.html"
+        }
+      }
+    })
+    
+    .state('app.wire_and_wire_product_grades', {
+      url: "/learning/wire_and_wire_product_grades",
+      views: {
+        'menuContent' :{
+          templateUrl: "templates/learning/wire_and_wire_product_grades.html"
+        }
+      }
+    })
+    
+    .state('app.castings_and_forgings_grades', {
+      url: "/learning/castings_and_forgings_grades",
+      views: {
+        'menuContent' :{
+          templateUrl: "templates/learning/castings_and_forgings_grades.html"
+        }
+      }
+    })
+    
+    .state('app.what_is_wire_rod', {
+      url: "/learning/what_is_wire_rod",
+      views: {
+        'menuContent' :{
+          templateUrl: "templates/learning/what_is_wire_rod.html"
+        }
+      }
+    })
+    
+    .state('app.anti-corrosion_materials_for_castings,_nuts_and_bolts', {
+      url: "/learning/anti-corrosion_materials_for_castings,_nuts_and_bolts",
+      views: {
+        'menuContent' :{
+          templateUrl: "templates/learning/anti-corrosion_materials_for_castings,_nuts_and_bolts.html"
+        }
+      }
+    })
+    
+    .state('app.chinese_steel_grades', {
+      url: "/learning/chinese_steel_grades",
+      views: {
+        'menuContent' :{
+          templateUrl: "templates/learning/chinese_steel_grades.html"
+        }
+      }
+    })
+    
+    .state('app.coated_cold_rolled_steel_european_equivalent_grades', {
+      url: "/learning/coated_cold_rolled_steel_european_equivalent_grades",
+      views: {
+        'menuContent' :{
+          templateUrl: "templates/learning/coated_cold_rolled_steel_european_equivalent_grades.html"
+        }
+      }
+    })
+    
+    .state('app.about_electrical_steel', {
+      url: "/learning/about_electrical_steel",
+      views: {
+        'menuContent' :{
+          templateUrl: "templates/learning/about_electrical_steel.html"
+        }
+      }
+    })
+    
+    .state('app.electrical_steel_international_standards', {
+      url: "/learning/electrical_steel_international_standards",
+      views: {
+        'menuContent' :{
+          templateUrl: "templates/learning/electrical_steel_international_standards.html"
+        }
+      }
     });
   // if none of the above states are matched, use this as the fallback
   $urlRouterProvider.otherwise('/app/dashboard');
